1a0ee2ae88d8c89d4fa7351b4504f38758999608
Dana Hanna <dana.hanna@gmail.com>
2017-12-17 15:40:59 -0500

    latest

--- StrangerThingsArduino/StrangerThingsArduino.ino
+++ StrangerThingsArduino/StrangerThingsArduino.ino
@@ -6,20 +6,17 @@ Author:  danah
 #include <FastLED.h>
 
 #define PIN 7
-//#define LEDS 443
-#define LEDS 300
-#define BRIGHTNESS 50
-#define KINDA_WHITE 25
+#define LEDS 100
+#define BRIGHTNESS 100
 
 CRGB leds[LEDS];
-CRGB kindaWhite = CRGB(KINDA_WHITE, KINDA_WHITE, KINDA_WHITE);
 String out = "";
 bool normal = false;
 
 void setup() {
   pinMode(PIN, OUTPUT);
   pinMode(5, OUTPUT);
-  FastLED.addLeds<WS2812B, PIN, GRB>(leds, LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.addLeds<WS2811, PIN, GRB>(leds, LEDS).setCorrection(TypicalLEDStrip);
   FastLED.setBrightness(BRIGHTNESS);
   Serial.begin(115200);
 }
@@ -36,10 +33,10 @@ void loop() {
       if (c != 'X') {
         Serial.write(c);
         c = c - 'a';
-        light(c);
+        light(c * 3+5);
         FastLED.show();
         delay(1000);
-        leds[c * 10] = CRGB::Black;
+        leds[c * 3+5] = CRGB::Black;
         FastLED.show();
         delay(100);
       }
@@ -51,7 +48,6 @@ void loop() {
     }
     departure();
   }
-  normal = false;
   normalColors(true);
   delay(5000);
 }
@@ -59,7 +55,7 @@ void loop() {
 void arrival() {
   normalColors(true);
   for (int j = 40; j >= 0; j--) {
-    for (int i = 0; i < 260; i = i + 10) {
+    for (int i = 0; i < LEDS; i++) {
       CHSV c = rgb2hsv_approximate(leds[i]);
       c.v = random(j * 6);
       leds[i] = c;
@@ -73,7 +69,7 @@ void departure() {
   for (int j = 0; j <= 40; j++) {
     normal = false;
     normalColors(false);
-    for (int i = 0; i < 260; i = i + 10) {
+    for (int i = 0; i < LEDS; i++) {
       CHSV c = rgb2hsv_approximate(leds[i]);
       c.v = random(j * 6);
       leds[i] = c;
@@ -81,12 +77,13 @@ void departure() {
     FastLED.show();
     delay(100);
   }
+  normal = false;
 }
 
 void normalColors(bool showThem) {
   if (!normal) {
     normal = true;
-    for (int i = 0; i < 26; i++) {
+    for (int i = 0; i < LEDS; i++) {
       light(i);
     }
     if (showThem)
@@ -97,25 +94,25 @@ void normalColors(bool showThem) {
 void light(uint8_t i) {
   switch (i % 7) {
       case 0:
-        leds[i * 10] = CRGB::Red;
+        leds[i] = CRGB::Red;
         break;
       case 1:
-        leds[i * 10] = CRGB::Orange;
+        leds[i] = CRGB::Orange;
         break;
       case 2:
-        leds[i * 10] = CRGB::Yellow;
+        leds[i] = CRGB::Yellow;
         break;
       case 3:
-        leds[i * 10] = CRGB::Green;
+        leds[i] = CRGB::Green;
         break;
       case 4:
-        leds[i * 10] = CRGB::Blue;
+        leds[i] = CRGB::Blue;
         break;
       case 5:
-        leds[i * 10] = CRGB::Purple;
+        leds[i] = CRGB::Purple;
         break;
       case 6:
-        leds[i * 10] = CRGB::Indigo;
+        leds[i] = CRGB::Indigo;
         break;
       }
 }
--- StrangerThingsPi/Program.cs
+++ StrangerThingsPi/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.IO.Ports;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -13,11 +15,32 @@ namespace StrangerThingsPi
 
         static void Main(string[] args)
         {
-            sp = new SerialPort("/dev/ttyAMA0", 115200, Parity.None, 8, StopBits.One);
-            sp.Open();
-            sp.Write("dana hanna");
-            sp.Close();
+            while (true) {
+                try
+                {
+                    sp = new SerialPort("/dev/ttyAMA0", 115200, Parity.None, 8, StopBits.One);
+                    sp.Open();
+
+                    HttpListener listen = new HttpListener();
+                    listen.Prefixes.Add("http://*:8080/");
+                    listen.Start();
+                    while (true)
+                    {
+                        var c = listen.GetContext();
+                        var msg = new StreamReader(c.Request.InputStream).ReadToEnd();
+                        Console.WriteLine(msg);
+                        var body = msg.Split(new string[] { "Body=" }, StringSplitOptions.None)[1].Split('&')[0].Replace('+', ' ').ToLower();
+                        Console.WriteLine(body);
+                        sp.Write(body);
+                    }
+                } catch
+                {
+                    try
+                    {
+                        sp.Close();
+                    } catch { }
+                }
+            }
         }
     }
-
 }
