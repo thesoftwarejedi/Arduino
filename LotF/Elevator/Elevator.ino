2b0e226cee46d2f7ad988795fd5923a96875fa4f
Dana Hanna <dana.hanna@gmail.com>
2018-09-02 11:25:36 -0400

    latest elevator

--- LotF/Elevator/Elevator.ino
+++ LotF/Elevator/Elevator.ino
@@ -1,3 +1,5 @@
+#include <DMXSerial.h>
+
 /*
  Name:		Elevator.ino
  Created:	8/20/2017 2:31:39 PM
@@ -5,293 +7,157 @@
 */
 #include <FastLED.h>
 
-#define PIN1 5
-#define LEDS 46
-#define BRIGHTNESS 5
-#define FADE_LEN 20
-#define MAX_SPEED 1
-#define MIN_SPEED 90
-#define ACCELERATION .7
-#define ACCEL_CONSTANT 1
-#define NUM_FLOORS 8
-#define FLOOR_LEN 75
-#define BLINK_ODDS 60
-#define MAIN_COLOR CRGB(200, 56, 0)
+//led pin
+#define PIN1 2
 
-//#define DEBUG 1
+//pin for a simple status LED
+#define ERROR_PIN 3
+
+#define LEDS 119
+
+//we start at channel 1
+#define DMXSTART 1
+
+//max channels, just use em all
+#define DMXLENGTH 512
+
+#define BRIGHTNESS 100
 
 CRGB leds[LEDS];
 CRGB black[LEDS];
-CRGB temp[LEDS];
-CRGB fade[FADE_LEN];
-CRGB fade2[FADE_LEN];
+
+uint8_t _blinkRate = 0;
+uint8_t _speed = 127;
+CRGB _color = CRGB::Black;
 
 void setup() {
+  //we want to probe - not be interrupted.  interrupts will kill the LED timings
+  DMXSerial.init(DMXProbe);
+  DMXSerial.maxChannel(DMXLENGTH);
   
-  #ifdef DEBUG
-  Serial.begin(9600);
-  #endif 
+  pinMode(PIN1, OUTPUT);
+  pinMode(ERROR_PIN, OUTPUT);
   
   FastLED.addLeds<WS2812B, PIN1, GRB>(leds, LEDS).setCorrection(TypicalLEDStrip);
 	FastLED.setBrightness(BRIGHTNESS);
 
-  setMainFloor(MAIN_COLOR);
-  grabFades();
-  
-  #ifdef DEBUG
-  for (uint8_t i = 0; i < FADE_LEN; i++) {
-    Serial.println(fade[i].red);
-    Serial.println(fade[i].green);
-    Serial.println(fade[i].blue);
-  }
-  for (uint8_t i = 0; i < FADE_LEN; i++) {
-    Serial.println(fade2[i].red);
-    Serial.println(fade2[i].green);
-    Serial.println(fade2[i].blue);
-  }
-  #endif 
+  //blink the LED on startup
+  digitalWrite(ERROR_PIN, HIGH);
+  delay(200);
+  digitalWrite(ERROR_PIN, LOW);
+  delay(200);
+  digitalWrite(ERROR_PIN, HIGH);
+  delay(200);
+  digitalWrite(ERROR_PIN, LOW);
+  delay(200);
+  digitalWrite(ERROR_PIN, HIGH);
+  delay(200);
+  digitalWrite(ERROR_PIN, LOW);
+
+  wholeStrip(CRGB::Black, true);
+  updateLeds();
 }
 
 void loop() {
-  setMainFloor(MAIN_COLOR);
-  for (uint8_t i = 0; i < 50; i++)
-  {
-    draw();
-    delay(100);
-  }
-  slideDown(MIN_SPEED, ACCELERATION * -1);
 
-  CHSV hsv;
-  CRGB color;
-  for (uint8_t i = 0; i < NUM_FLOORS; i++) {
-    delay(500);
-    hsv = CHSV(30 - (30 / NUM_FLOORS) * (i + 1), 255, 255); 
-    hsv2rgb_rainbow(hsv, color);
-    drawFloor(color, FLOOR_LEN, MAX_SPEED, 0);
-    slideDown(MAX_SPEED, 0);
-  }
-
-  drawFloor(color, LEDS - FADE_LEN * 2, MAX_SPEED, ACCELERATION);
+  //1 - on/off
+  //2,3,4 - color
+  //5 speed 127 = stationary
+  //6 mode = stopped on floor, starting, moving, stopping
+  //7 flickering rate % 
+  //8 floor height
+  //9 distance between floors
   
-  while(true)
-  {
-    draw();
-    delay(100);
-  }
-}
-
-void setMainFloor(CRGB color) {
-  uint8_t fadeAmt;
-  wholeStrip(color);
-  for (uint8_t i = 0; i < FADE_LEN; i++) {
-    fadeAmt = 255 - (uint8_t)((float)i / FADE_LEN * 256);
-
-    #ifdef DEBUG
-    Serial.print("led ");
-    Serial.print(i);
-    Serial.print(" fade ");
-    Serial.println(fadeAmt);
-    #endif 
+  if (DMXSerial.receive(100)) {
+    //status pin off
+    digitalWrite(ERROR_PIN, HIGH); 
     
-    leds[i].fadeToBlackBy(fadeAmt);
-    leds[LEDS - 1 - i].fadeToBlackBy(fadeAmt);
+    //get data
+    uint8_t *data = DMXSerial.getBuffer();
+    
+    //if set to off
+    if (data[DMXSTART] < 128) {
+      wholeStrip(CRGB::Black, true);
+      updateLeds();
+      delay(250);
+      return;
+    }
+
+    //we are on
+    
+    //grab color
+    _color = CRGB(data[DMXSTART+1], data[DMXSTART+2], data[DMXSTART+3]);
+
+    //grab blink %
+    _blinkRate = data[7];
+
+    //get speed
+    _speed = 255 - data[5];
+    
+    if (data[6] < 64 ) { //set to stable
+      wholeStrip(_color, false);
+      draw();
+      delay(_speed); //
+      return;
+    } 
+
+    //while doing these, we do not re-read from DMX
+    //moving does one lap, then re-reads
+    if (data[6] < 128) { //starting
+      
+    } else if (data[6] < 192) { //moving
+      wholeStrip(CRGB::Black, true);
+      //for (uint8_t = 
+    } else { //stopping
+      
+    }
+  
+  } else {
+    wholeStrip(CRGB::Black, true);
+    updateLeds();
+    digitalWrite(ERROR_PIN, LOW);
   } 
 }
 
-void grabFades() {
-  uint16_t len = FADE_LEN * sizeof(CRGB);  
-  memcpy(fade, leds, len);
-  for (uint8_t i = 0; i < FADE_LEN; i++) {
-    fade2[i] = fade[FADE_LEN - 1 - i];
-  }
-}
-
-void drawFloor(CRGB color, uint16_t len, float curSpeed, float delta) {
-  //kinda lame, but draw the whole thing and grabe the fades, then clear
-  setMainFloor(color);
-  grabFades();
-  wholeStrip(CRGB(0,0,0));
-
-  int16_t whenToStartDelta = LEDS - abs((MIN_SPEED - MAX_SPEED) / delta);
-
-  #ifdef DEBUG
-  Serial.println(whenToStartDelta);
-  #endif
-
-  float newSpeed;
-  for (uint8_t i = 0; i < FADE_LEN; i++) {
-    delay(curSpeed * ACCEL_CONSTANT - ACCEL_CONSTANT + 1);    
-
-    #ifdef DEBUG
-    Serial.println(curSpeed);
-    #endif
-    
-    newSpeed = curSpeed + delta;
-    if (newSpeed > MIN_SPEED)
-      curSpeed = MIN_SPEED;
-    else if (newSpeed < MAX_SPEED)
-      curSpeed = MAX_SPEED;
-    else if (whenToStartDelta-- < 0)
-      curSpeed = newSpeed;
-
-    #ifdef DEBUG
-    Serial.println(whenToStartDelta);
-    #endif
-    
-    scrollDown();
-    setLed(0, fade2[FADE_LEN - i - 1]);
-    draw();
-  }
-  for (uint8_t i = 0; i < len; i++) {
-    delay(curSpeed * ACCEL_CONSTANT - ACCEL_CONSTANT + 1); 
-
-    #ifdef DEBUG
-    Serial.println(curSpeed);
-    #endif
-    
-    newSpeed = curSpeed + delta;
-    if (newSpeed > MIN_SPEED)
-      curSpeed = MIN_SPEED;
-    else if (newSpeed < MAX_SPEED)
-      curSpeed = MAX_SPEED;
-    else if (whenToStartDelta-- < 0)
-      curSpeed = newSpeed;
-
-    #ifdef DEBUG
-    Serial.println(whenToStartDelta);
-    #endif
-      
-    scrollDown();
-    setLed(0, color);
-    draw(); 
-  }
-  
-  for (uint8_t i = 0; i < FADE_LEN; i++) {
-    delay(curSpeed * ACCEL_CONSTANT - ACCEL_CONSTANT + 1); 
-
-    #ifdef DEBUG
-    Serial.println(curSpeed);
-    #endif
-    
-    newSpeed = curSpeed + delta;
-    if (newSpeed > MIN_SPEED)
-      curSpeed = MIN_SPEED;
-    else if (newSpeed < MAX_SPEED)
-      curSpeed = MAX_SPEED;
-    else if (whenToStartDelta-- < 0)
-      curSpeed = newSpeed;
-
-    #ifdef DEBUG
-    Serial.println(whenToStartDelta);
-    #endif
-      
-    scrollDown();
-    setLed(0, fade[FADE_LEN - i - 1]);
-    draw();
-  }
-}
-
 void scrollDown() {
-	for (uint16_t i = LEDS - 1; i >= 1; i--)
+	for (uint8_t i = LEDS - 1; i >= 1; i--)
 		setLed(i, getLed(i - 1));
 	setLed(0, CRGB::Black);
 }
 
-/*
 void scrollUp() {
 	for (uint16_t i = 0; i < LEDS - 1; i++)
 		setLed(i, getLed(i + 1));
 	setLed(LEDS - 1, CRGB::Black);
 }
-*/
 
-void slideDown(float curSpeed, float delta) {
-  int16_t whenToStartDelta = LEDS - 50 - abs((MIN_SPEED - MAX_SPEED) / delta);
-  
-  float newSpeed;
-	for (uint16_t i = LEDS - 1; i > 0; i--) {
-		setLed(0, CRGB::Black);
-		scrollDown();
-		draw();
-		delay(curSpeed * ACCEL_CONSTANT - ACCEL_CONSTANT + 1); 
-
-    #ifdef DEBUG
-    Serial.println(curSpeed);
-    #endif
-    
-    newSpeed = curSpeed + delta;
-
-    #ifdef DEBUG
-    Serial.println(newSpeed);
-    #endif
-    
-    if (newSpeed > MIN_SPEED)
-      curSpeed = MIN_SPEED;
-    else if (newSpeed < MAX_SPEED)
-      curSpeed = MAX_SPEED;
-    else if (whenToStartDelta-- < 0)
-      curSpeed = newSpeed;
-
-    #ifdef DEBUG
-    Serial.println(whenToStartDelta);
-    #endif
-	}
-}
-
-/*
-void slideUp(float curSpeed) {
-	for (uint16_t i = 0; i < LEDS - 1; i++) {
-		setLed(LEDS - 1, CRGB::Black);
-		scrollUp();
-		draw();
-		delay(curSpeed * ACCEL_CONSTANT - ACCEL_CONSTANT + 1);
-	}
-}
-*/
-
-void wholeStrip(CRGB color) {
-	for (uint16_t i = 0; i < LEDS; i++) {
-   leds[i] = color;
-   temp[i] = color;
+void wholeStrip(CRGB color, bool overrideBlink) {
+	for (uint8_t i = 0; i < LEDS; i++) {
+      setLed(i, color);
 	}
 }
 
 bool _isBlack = false;
 
-void setLed(uint16_t led, CRGB color) {
-  led = LEDS - 1 - led;
-  if (_isBlack) {
-    temp[led] = color;
-  } else {
+void setLed(uint8_t led, CRGB color) {
     leds[led] = color;
-  }
 }
 
-CRGB getLed(uint16_t led) {
-  led = LEDS - 1 - led;
-  if (_isBlack) {
-    return temp[led];
-  } else {
+CRGB getLed(uint8_t led) {
     return leds[led];
-  }
 }
 
 void draw() {
-  bool wasBlack = _isBlack;
-  if (random(0, 100) < BLINK_ODDS) {
-    _isBlack = true;
-    if (!wasBlack) {
-      memcpy(temp, leds, LEDS * sizeof(CRGB));
-      memcpy(leds, black, LEDS * sizeof(CRGB));
-      FastLED.show();
-    }
-  } else  {
-    if (wasBlack) {
-      memcpy(leds, temp, LEDS * sizeof(CRGB));
-    }
-     _isBlack = false;
-    FastLED.show();
+  if (random(0, 255) < _blinkRate) {
+    FastLED.addLeds<WS2812B, PIN1, GRB>(black, LEDS).setCorrection(TypicalLEDStrip);
+  } else {
+    FastLED.addLeds<WS2812B, PIN1, GRB>(leds, LEDS).setCorrection(TypicalLEDStrip);
   }
+  updateLeds();
+}
+
+void updateLeds() {
+  FastLED.show();
 }
 
 
---
+++ LotF/ElevatorPreprogrammed/ElevatorPreprogrammed.ino
@@ -0,0 +1,297 @@
+/*
+ Name:		Elevator.ino
+ Created:	8/20/2017 2:31:39 PM
+ Author:	danah
+*/
+#include <FastLED.h>
+
+#define PIN1 2
+#define LEDS 119
+#define BRIGHTNESS 100
+#define FADE_LEN 10
+#define MAX_SPEED 1
+#define MIN_SPEED 90
+#define ACCELERATION .7
+#define ACCEL_CONSTANT 1
+#define NUM_FLOORS 8
+#define FLOOR_LEN 75
+#define BLINK_ODDS 8
+#define MAIN_COLOR CRGB(200, 56, 0)
+
+//#define DEBUG 1
+
+CRGB leds[LEDS];
+CRGB black[LEDS];
+CRGB temp[LEDS];
+CRGB fade[FADE_LEN];
+CRGB fade2[FADE_LEN];
+
+void setup() {
+  
+  #ifdef DEBUG
+  Serial.begin(9600);
+  #endif 
+  
+  FastLED.addLeds<WS2812B, PIN1, GRB>(leds, LEDS).setCorrection(TypicalLEDStrip);
+	FastLED.setBrightness(BRIGHTNESS);
+
+  setMainFloor(MAIN_COLOR);
+  grabFades();
+  
+  #ifdef DEBUG
+  for (uint8_t i = 0; i < FADE_LEN; i++) {
+    Serial.println(fade[i].red);
+    Serial.println(fade[i].green);
+    Serial.println(fade[i].blue);
+  }
+  for (uint8_t i = 0; i < FADE_LEN; i++) {
+    Serial.println(fade2[i].red);
+    Serial.println(fade2[i].green);
+    Serial.println(fade2[i].blue);
+  }
+  #endif 
+}
+
+void loop() {
+  setMainFloor(MAIN_COLOR);
+  for (uint8_t i = 0; i < 100; i++)
+  {
+    draw();
+    delay(100);
+  }
+  slideDown(MIN_SPEED, ACCELERATION * -1);
+
+  CHSV hsv;
+  CRGB color;
+  for (uint8_t i = 0; i < NUM_FLOORS; i++) {
+    delay(500);
+    hsv = CHSV(30 - (30 / NUM_FLOORS) * (i + 1), 255, 255); 
+    hsv2rgb_rainbow(hsv, color);
+    drawFloor(color, FLOOR_LEN, MAX_SPEED, 0);
+    slideDown(MAX_SPEED, 0);
+  }
+
+  drawFloor(color, LEDS - FADE_LEN * 2, MAX_SPEED, ACCELERATION);
+  
+  for (uint8_t i = 0; i < 100; i++)
+  {
+    draw();
+    delay(100);
+  }
+}
+
+void setMainFloor(CRGB color) {
+  uint8_t fadeAmt;
+  wholeStrip(color);
+  for (uint8_t i = 0; i < FADE_LEN; i++) {
+    fadeAmt = 255 - (uint8_t)((float)i / FADE_LEN * 256);
+
+    #ifdef DEBUG
+    Serial.print("led ");
+    Serial.print(i);
+    Serial.print(" fade ");
+    Serial.println(fadeAmt);
+    #endif 
+    
+    leds[i].fadeToBlackBy(fadeAmt);
+    leds[LEDS - 1 - i].fadeToBlackBy(fadeAmt);
+  } 
+}
+
+void grabFades() {
+  uint16_t len = FADE_LEN * sizeof(CRGB);  
+  memcpy(fade, leds, len);
+  for (uint8_t i = 0; i < FADE_LEN; i++) {
+    fade2[i] = fade[FADE_LEN - 1 - i];
+  }
+}
+
+void drawFloor(CRGB color, uint16_t len, float curSpeed, float delta) {
+  //kinda lame, but draw the whole thing and grabe the fades, then clear
+  setMainFloor(color);
+  grabFades();
+  wholeStrip(CRGB(0,0,0));
+
+  int16_t whenToStartDelta = LEDS - abs((MIN_SPEED - MAX_SPEED) / delta);
+
+  #ifdef DEBUG
+  Serial.println(whenToStartDelta);
+  #endif
+
+  float newSpeed;
+  for (uint8_t i = 0; i < FADE_LEN; i++) {
+    delay(curSpeed * ACCEL_CONSTANT - ACCEL_CONSTANT + 1);    
+
+    #ifdef DEBUG
+    Serial.println(curSpeed);
+    #endif
+    
+    newSpeed = curSpeed + delta;
+    if (newSpeed > MIN_SPEED)
+      curSpeed = MIN_SPEED;
+    else if (newSpeed < MAX_SPEED)
+      curSpeed = MAX_SPEED;
+    else if (whenToStartDelta-- < 0)
+      curSpeed = newSpeed;
+
+    #ifdef DEBUG
+    Serial.println(whenToStartDelta);
+    #endif
+    
+    scrollDown();
+    setLed(0, fade2[FADE_LEN - i - 1]);
+    draw();
+  }
+  for (uint8_t i = 0; i < len; i++) {
+    delay(curSpeed * ACCEL_CONSTANT - ACCEL_CONSTANT + 1); 
+
+    #ifdef DEBUG
+    Serial.println(curSpeed);
+    #endif
+    
+    newSpeed = curSpeed + delta;
+    if (newSpeed > MIN_SPEED)
+      curSpeed = MIN_SPEED;
+    else if (newSpeed < MAX_SPEED)
+      curSpeed = MAX_SPEED;
+    else if (whenToStartDelta-- < 0)
+      curSpeed = newSpeed;
+
+    #ifdef DEBUG
+    Serial.println(whenToStartDelta);
+    #endif
+      
+    scrollDown();
+    setLed(0, color);
+    draw(); 
+  }
+  
+  for (uint8_t i = 0; i < FADE_LEN; i++) {
+    delay(curSpeed * ACCEL_CONSTANT - ACCEL_CONSTANT + 1); 
+
+    #ifdef DEBUG
+    Serial.println(curSpeed);
+    #endif
+    
+    newSpeed = curSpeed + delta;
+    if (newSpeed > MIN_SPEED)
+      curSpeed = MIN_SPEED;
+    else if (newSpeed < MAX_SPEED)
+      curSpeed = MAX_SPEED;
+    else if (whenToStartDelta-- < 0)
+      curSpeed = newSpeed;
+
+    #ifdef DEBUG
+    Serial.println(whenToStartDelta);
+    #endif
+      
+    scrollDown();
+    setLed(0, fade[FADE_LEN - i - 1]);
+    draw();
+  }
+}
+
+void scrollDown() {
+	for (uint16_t i = LEDS - 1; i >= 1; i--)
+		setLed(i, getLed(i - 1));
+	setLed(0, CRGB::Black);
+}
+
+/*
+void scrollUp() {
+	for (uint16_t i = 0; i < LEDS - 1; i++)
+		setLed(i, getLed(i + 1));
+	setLed(LEDS - 1, CRGB::Black);
+}
+*/
+
+void slideDown(float curSpeed, float delta) {
+  int16_t whenToStartDelta = LEDS - 50 - abs((MIN_SPEED - MAX_SPEED) / delta);
+  
+  float newSpeed;
+	for (uint16_t i = LEDS - 1; i > 0; i--) {
+		setLed(0, CRGB::Black);
+		scrollDown();
+		draw();
+		delay(curSpeed * ACCEL_CONSTANT - ACCEL_CONSTANT + 1); 
+
+    #ifdef DEBUG
+    Serial.println(curSpeed);
+    #endif
+    
+    newSpeed = curSpeed + delta;
+
+    #ifdef DEBUG
+    Serial.println(newSpeed);
+    #endif
+    
+    if (newSpeed > MIN_SPEED)
+      curSpeed = MIN_SPEED;
+    else if (newSpeed < MAX_SPEED)
+      curSpeed = MAX_SPEED;
+    else if (whenToStartDelta-- < 0)
+      curSpeed = newSpeed;
+
+    #ifdef DEBUG
+    Serial.println(whenToStartDelta);
+    #endif
+	}
+}
+
+/*
+void slideUp(float curSpeed) {
+	for (uint16_t i = 0; i < LEDS - 1; i++) {
+		setLed(LEDS - 1, CRGB::Black);
+		scrollUp();
+		draw();
+		delay(curSpeed * ACCEL_CONSTANT - ACCEL_CONSTANT + 1);
+	}
+}
+*/
+
+void wholeStrip(CRGB color) {
+	for (uint16_t i = 0; i < LEDS; i++) {
+   leds[i] = color;
+   temp[i] = color;
+	}
+}
+
+bool _isBlack = false;
+
+void setLed(uint16_t led, CRGB color) {
+  led = LEDS - 1 - led;
+  if (_isBlack) {
+    temp[led] = color;
+  } else {
+    leds[led] = color;
+  }
+}
+
+CRGB getLed(uint16_t led) {
+  led = LEDS - 1 - led;
+  if (_isBlack) {
+    return temp[led];
+  } else {
+    return leds[led];
+  }
+}
+
+void draw() {
+  bool wasBlack = _isBlack;
+  if (random(0, 100) < BLINK_ODDS) {
+    _isBlack = true;
+    if (!wasBlack) {
+      memcpy(temp, leds, LEDS * sizeof(CRGB));
+      memcpy(leds, black, LEDS * sizeof(CRGB));
+      FastLED.show();
+    }
+  } else  {
+    if (wasBlack) {
+      memcpy(leds, temp, LEDS * sizeof(CRGB));
+    }
+     _isBlack = false;
+    FastLED.show();
+  }
+}
+
+
