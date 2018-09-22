ff52eec1c6491d23a9a19be31fb73fd04344b561
Dana Hanna <dana.hanna@gmail.com>
2018-09-22 13:13:57 -0400

    implemented blinking

--- LotF/Eyes/randomeyes/randomeyes.ino
+++ LotF/Eyes/randomeyes/randomeyes.ino
@@ -1,3 +1,5 @@
+#include <QueueList.h>
+
 #include <FastLED.h>
 #include <fastled_config.h>
 #include <fastled_delay.h>
@@ -24,22 +26,26 @@
 //seconds until completely on
 #define SECONDS_UNTIL_ON 15
 //seconds to remain completely on
-#define SECONDS_PAUSE_ON 3
-//a percent of randomness to add to the wait between eyes tunring on
-#define RANDOM_PCT 50
+#define SECONDS_PAUSE_ON 30
+//a percent of randomness to add to the wait between eyes turning on
+#define RANDOM_PCT 70
+//how often an eye blinks
+#define BLINK_EVERY_MILLIS 10000
+//how long a blink lasts
+#define BLINK_LENGTH 100
 
 /*
  * COLOR MIN AND MAX
  */
-#define MAX_RED 35
-#define MIN_RED 20
+#define MAX_RED 25
+#define MIN_RED 10
 #define MAX_GREEN 0
 #define MIN_GREEN 0
 #define MAX_BLUE 0
 #define MIN_BLUE 0
 
 //the number of eyes in the line
-#define NUM_EYES 8*12
+#define NUM_EYES 8*8
 
 /*
  * don't change these
@@ -58,8 +64,22 @@ CRGB leds[NUM_LEDS];
 
 uint8_t eyeOrder[NUM_EYES];
 
+struct Blink {
+  unsigned long endTime;
+  uint8_t eye;
+  CRGB color;
+};
+
+QueueList<Blink> queue;
+
+//frequency to check for blink (lower is higher resolution)
+#define BLINK_REFRESH_RATE 10
+
+//record start time
+unsigned long start = millis();
+
 void setup () {  
-  //enable serial in case we want debug statements
+  //for debugging perhaps
   Serial.begin(115200);
   
   pinMode(DATA_PIN, OUTPUT);
@@ -69,12 +89,15 @@ void setup () {
   digitalWrite(TRIGGER_G_PIN, LOW);
 
   //add LEDs to FastLED library
-  FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
+  //strip style
+  FastLED.addLeds<WS2811, DATA_PIN, GRB>(leds, NUM_LEDS);
+  //cmas style
+  //FastLED.addLeds<WS2811, DATA_PIN, RGB>(leds, NUM_LEDS);
   
   off();
   updateLeds();
   
-  //initialize the order to turn on the eyes to the index; shuffle later
+  //init to the index
   for (uint8_t i = 0; i < NUM_EYES; i++) {
     eyeOrder[i] = i;
   }
@@ -82,9 +105,10 @@ void setup () {
   randomSeed(analogRead(A0));
 }
 
+bool blinkEnabled = false;
+
 void loop() {  
-  
-  //shuffle the order to show the eyes
+  //shuffle
   for (uint8_t i = 0; i < NUM_EYES; i++) {
     int n = random(0, NUM_EYES);
     uint8_t temp = eyeOrder[n];
@@ -92,12 +116,15 @@ void loop() {
     eyeOrder[i] = temp;
   }
 
-  //come up with our target per eye wait time
+  //computer the time to wait between lights (randomized later)
   float wait = (float)1 / (float)(NUM_EYES / (float)SECONDS_UNTIL_ON) * (float)1000;
+  
+  Serial.println(wait);
   
   CRGB color;
 
-  //go
+  //now go
+  blinkEnabled = true;
   for (uint8_t c = 0; c < NUM_EYES; c++) {
     uint8_t c1 = random(MIN_RED, MAX_RED);
     uint8_t c2 = random(MIN_GREEN, MAX_GREEN);
@@ -105,14 +132,15 @@ void loop() {
     color = CRGB(c1, c2, c3);
     showEye(eyeOrder[c], color);
     updateLeds();
-    
     float curWait = random(wait - (wait * (float)((float)RANDOM_PCT / 100)), wait + (wait * (float)((float)RANDOM_PCT / 100)));
-    //todo: make some eye blinking happen here
-    delay(curWait);
+
+    delayWithUpdate(curWait);
   }
-  
-  //todo: make some eye blinking happen here too
-  delay(SECONDS_PAUSE_ON * 1000);
+
+  delayWithUpdate(SECONDS_PAUSE_ON * 1000);
+
+  blinkEnabled = false;
+  queue = QueueList<Blink>();
   
   off();
   updateLeds();
@@ -130,7 +158,35 @@ void off() {
   }
 }
 
+unsigned long nextBlink = 0;
+
 void updateLeds() {
+  if (blinkEnabled && millis() > nextBlink) {
+    
+    //check for a light to blink
+    for (uint8_t i = 0; i < NUM_EYES; i++) {
+      //if the eye is on
+      if (leds[i*2].r > 0 || leds[i*2].g > 0 || leds[i*2].b > 0) {
+        //if the odds are to blink
+        if (random(0, BLINK_EVERY_MILLIS / BLINK_REFRESH_RATE) == 0) {
+          //add the eye to our queue to turn back on
+          queue.push({millis() + BLINK_LENGTH, i, leds[i*2]});
+          //black the eye
+          showEye(i, CRGB::Black);
+        }
+      }
+    }
+
+    //check for a light to unblink
+    while (!queue.isEmpty() && millis() > queue.peek().endTime) {
+      //grab the info and set the eye back to the color it was
+      Blink b = queue.pop();
+      showEye(b.eye, b.color);
+    }
+    
+    //set our time to check blink next
+    nextBlink = millis() + BLINK_REFRESH_RATE;
+  }
   FastLED.show();
 }
 
@@ -138,6 +194,17 @@ void showEye(uint8_t i, CRGB c) {
   uint16_t li = i * 2;
   showLed(li, c);
   showLed(li + 1, c);
+}
+
+void delayWithUpdate(unsigned long ms) {
+  
+  Serial.println(ms);
+  
+  unsigned long pauseEnd = millis() + ms;
+  while (millis() < pauseEnd) {
+    delay(10);
+    updateLeds();
+  }
 }
 
 void showLed(uint16_t i, CRGB c) {
