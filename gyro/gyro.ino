5187f1ec55acd9c9c1f78ba3dfee407f7394eee5
Dana Hanna <dana.hanna@gmail.com>
2018-11-25 19:45:39 -0500

    initial

--- gyro/gyro.ino
+++ gyro/gyro.ino
@@ -1,9 +1,142 @@
+#include <bitswap.h>
+#include <chipsets.h>
+#include <color.h>
+#include <colorpalettes.h>
+#include <colorutils.h>
+#include <controller.h>
+#include <cpp_compat.h>
+#include <dmx.h>
+#include <FastLED.h>
+#include <fastled_config.h>
+#include <fastled_delay.h>
+#include <fastled_progmem.h>
+#include <fastpin.h>
+#include <fastspi.h>
+#include <fastspi_bitbang.h>
+#include <fastspi_dma.h>
+#include <fastspi_nop.h>
+#include <fastspi_ref.h>
+#include <fastspi_types.h>
+#include <hsv2rgb.h>
+#include <led_sysdefs.h>
+#include <lib8tion.h>
+#include <noise.h>
+#include <pixelset.h>
+#include <pixeltypes.h>
+#include <platforms.h>
+#include <power_mgt.h>
+
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_ADXL345_U.h>
 
 /* Assign a unique ID to this sensor at the same time */
 Adafruit_ADXL345_Unified accel = Adafruit_ADXL345_Unified(12345);
+
+#define BUTTON_PIN 6
+#define LED_PIN 8
+#define NUM_LEDS 70
+
+//our LEDs
+CRGB leds[NUM_LEDS];
+
+void setup(void) 
+{
+#ifndef ESP8266
+  while (!Serial); // for Leonardo/Micro/Zero
+#endif
+  Serial.begin(9600);
+
+  //button init
+  pinMode(BUTTON_PIN, INPUT_PULLUP);
+
+  //led init
+  pinMode(LED_PIN, OUTPUT);
+  FastLED.addLeds<WS2811, LED_PIN, RGB>(leds, NUM_LEDS);
+  FastLED.setBrightness(10);
+
+  //accel init
+  Serial.println("Accelerometer Test"); Serial.println("");
+  
+  /* Initialise the sensor */
+  if(!accel.begin())
+  {
+    /* There was a problem detecting the ADXL345 ... check your connections */
+    Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
+    while(1);
+  }
+
+  /* Set the range to whatever is appropriate for your project */
+  accel.setRange(ADXL345_RANGE_16_G);
+  // accel.setRange(ADXL345_RANGE_8_G);
+  // accel.setRange(ADXL345_RANGE_4_G);
+  // accel.setRange(ADXL345_RANGE_2_G);
+  
+  /* Display some basic information on this sensor */
+  displaySensorDetails();
+  
+  /* Display additional settings (outside the scope of sensor_t) */
+  displayDataRate();
+  displayRange();
+  Serial.println("");
+}
+
+void loop(void) 
+{
+  /* Get a new sensor event */ 
+  sensors_event_t event; 
+  accel.getEvent(&event);
+ 
+  /* Display the results (acceleration is measured in m/s^2) */
+  
+  //Serial.print("X: "); Serial.print(event.acceleration.x); Serial.println("  ");
+  /*
+  Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("  ");
+  Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print("  ");Serial.println("m/s^2 ");
+
+  Serial.print("Button:"); Serial.println(digitalRead(BUTTON_PIN));
+*/
+
+  //play with led
+  for (uint8_t i = 0; i < NUM_LEDS; i++) {
+    leds[i] = CRGB::Black;
+  }
+
+  bool upsideDown = event.acceleration.z < 0;
+
+  float pct = ((float)(event.acceleration.x + 10)) * 5 / 100;
+  if (pct < 0) pct = 0;
+  if (pct > 100) pct = 100;
+
+  if (upsideDown) {
+    pct = 100 - pct;
+  }
+  
+  uint8_t lts = (uint8_t)(pct * NUM_LEDS);
+
+  displayLed(lts-3, CRGB(50,0,0));
+  displayLed(lts-2, CRGB(0,50,0));
+  displayLed(lts-1, CRGB(50,0,0));
+  displayLed(lts, CRGB(0,50,0));
+  displayLed(lts+1, CRGB(50,0,0));
+  displayLed(lts+2, CRGB(0,50,0));
+  displayLed(lts+3, CRGB(50,0,0));
+  
+  FastLED.show();
+  
+  delay(50);
+}
+
+void displayLed(uint8_t led, CRGB color) {
+  if (led < 0) {
+    led += NUM_LEDS;
+  } else if (led > NUM_LEDS - 1) {
+    led -= NUM_LEDS;
+  }
+  //reverse
+  led = NUM_LEDS - 1 - led;
+  leds[led] = color;
+}
 
 void displaySensorDetails(void)
 {
@@ -105,48 +238,4 @@ void displayRange(void)
       break;
   }  
   Serial.println(" g");  
-}
-
-void setup(void) 
-{
-#ifndef ESP8266
-  while (!Serial); // for Leonardo/Micro/Zero
-#endif
-  Serial.begin(9600);
-  Serial.println("Accelerometer Test"); Serial.println("");
-  
-  /* Initialise the sensor */
-  if(!accel.begin())
-  {
-    /* There was a problem detecting the ADXL345 ... check your connections */
-    Serial.println("Ooops, no ADXL345 detected ... Check your wiring!");
-    while(1);
-  }
-
-  /* Set the range to whatever is appropriate for your project */
-  accel.setRange(ADXL345_RANGE_16_G);
-  // accel.setRange(ADXL345_RANGE_8_G);
-  // accel.setRange(ADXL345_RANGE_4_G);
-  // accel.setRange(ADXL345_RANGE_2_G);
-  
-  /* Display some basic information on this sensor */
-  displaySensorDetails();
-  
-  /* Display additional settings (outside the scope of sensor_t) */
-  displayDataRate();
-  displayRange();
-  Serial.println("");
-}
-
-void loop(void) 
-{
-  /* Get a new sensor event */ 
-  sensors_event_t event; 
-  accel.getEvent(&event);
- 
-  /* Display the results (acceleration is measured in m/s^2) */
-  Serial.print("X: "); Serial.print(event.acceleration.x); Serial.print("  ");
-  Serial.print("Y: "); Serial.print(event.acceleration.y); Serial.print("  ");
-  Serial.print("Z: "); Serial.print(event.acceleration.z); Serial.print("  ");Serial.println("m/s^2 ");
-  delay(500);
 }
